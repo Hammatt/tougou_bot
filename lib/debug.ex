4bb2c6018744d15e639e7714ad4b98ff98f86898
Alexander Hammatt <alexander.hammatt@protonmail.com>
2017-07-28 12:27:14 +1200

    added help command to list all commands.

--- lib/debug.ex
+++ lib/debug.ex
@@ -28,4 +28,42 @@ defmodule TougouBot.Debug do
     Cogs.say("Tougou Version: "<>vsn<>" reporting in!\nUptime: "<>uptime()<>"\nMemory: "<>memory<>
             "Mb\nIO in: "<>io_in<>"Mb\nIO out: "<>io_out<>"Mb")
   end
+
+  Cogs.def help do
+    cmds = Enum.map(Cogs.all_commands, fn ({k, _}) -> k end)
+    Cogs.say "```"<>descriptions_from_cmds(cmds)<>"```"
+  end
+
+  defp descriptions_from_cmds([]) do
+    ""
+  end
+  defp descriptions_from_cmds([head | tail]) do
+    case command_descriptions[head] do
+      nil ->
+        "Developer forgot to describe "<>head<>",\n"<>descriptions_from_cmds(tail)
+      d ->
+        d<>"\n"<>descriptions_from_cmds(tail)
+    end
+  end
+
+  defp command_descriptions do
+    descriptions = 
+    %{
+      "ping" => "!ping: tougou-chan should reply with pong!",
+      "status" => "!status: tougou-chan will tell you about her running version, "<>
+                  "her uptime, and her memory/io stats.",
+      "jisho" => "!jisho <word>: tougou-chan will check jisho for the first word "<>
+                  "you give her and tell you its reading/meaning.",
+      "vndb" => "!vndb <term>: tougou-chan will give you the most popular vn "<>
+                "that she can find using the term on vndb.",
+      "vndbrng" => "!vndbrng: tougou-chan will give you a random vn from vndb",
+      "tag" => "!tag <tag>: tougou-chan will attempt to recall the content "<>
+                "associated with the given tag",
+      "ntag" => "!ntag <tag> <content>: tougou-chan will learn and remember"<>
+                " a new tag->content pair.",
+      "dtag" => "!dtag <tag>: tougou-chan will forget the specified tag",
+      "atags" => "!atags: gives a list of all `tags` that tougou-chan knows",
+      "help" => "!help: gives a list of all commands and their descriptions"
+    }
+  end
 end
