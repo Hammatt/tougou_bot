62dd1b9375d4a6ddf69866fece1e329d599c1481
Alexander Hammatt <alexander.hammatt@protonmail.com>
2017-07-30 22:39:25 +1200

    made status command look nicer

--- lib/debug.ex
+++ lib/debug.ex
@@ -22,13 +22,11 @@ defmodule TougouBot.Debug do
   end
 
   Cogs.def status do
-    vsn = Mix.Project.config[:version]
-    memory = to_string(:erlang.memory()[:total] /1000000)
-    { { _, io_in }, { _, io_out } } = :erlang.statistics(:io)
-    io_in = to_string(io_in/1000000)
-    io_out = to_string(io_out/1000000)
-    Cogs.say("Tougou Version: "<>vsn<>" reporting in!\nUptime: "<>uptime()<>"\nMemory: "<>memory<>
-            "Mb\nIO in: "<>io_in<>"Mb\nIO out: "<>io_out<>"Mb")
+    %Embed { color: 0xFFB6C1 }
+    |> field("Version:", Mix.Project.config[:version])
+    |> field("Uptime:", uptime())
+    |> field("Memory use:", to_string(:erlang.memory()[:total] /1000000)<>"Mb")
+    |> Embed.send
   end
 
   Cogs.def help do
