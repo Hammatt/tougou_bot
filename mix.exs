fe9b02f38ae3d08cd67173ef96b18388cd015fb8
Alexander Hammatt <alexander.hammatt@protonmail.com>
2017-09-13 12:15:51 +1200

    bump version to 0.2.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TougouBot.Mixfile do
   def project do
     [
       app: :tougou_bot,
-      version: "0.2.6",
+      version: "0.2.7",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
