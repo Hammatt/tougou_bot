e9ce013f9cf516a8df0d56d54fb557bbdfb9a909
Alexander Hammatt <alexander.hammatt@protonmail.com>
2017-10-02 13:05:11 +1300

    made danbooru feature use embeds to be neater.

--- lib/modules/danbooru.ex
+++ lib/modules/danbooru.ex
@@ -3,28 +3,36 @@ defmodule TougouBot.Modules.Danbooru do
   TODO: moduledoc
   """
   use Alchemy.Cogs
+  alias Alchemy.Embed
+  import Embed
 
   Cogs.def pic do
     # random pic
-    Cogs.say(search(""))
+    Embed.send(search(""))
   end
 
   Cogs.set_parser(:pic, &TougouBot.Util.Parsers.plus_parser/1)
   Cogs.def pic(args) do
     #pic based on args
-    Cogs.say(search(args))
+    Embed.send(search(args))
   end
+
+  @danbooru_colour_embed %Embed{color: 0xADD8E6}
+  @danbooru_error_embed %Embed{color: 0xff0000}
 
   defp search(tags) do
     HTTPoison.start()
     case HTTPoison.get("https://danbooru.donmai.us/posts.json?search&random=true&limit=1&tags="<>tags) do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         data = Poison.decode!(body)
-        case data == nil do
+        case List.first(data) == nil do
           true ->
-            "それは居ない"#that doesnt exist
+            @danbooru_error_embed
+            |> title("それは居ない")#that doesnt exist
           _ ->
-            "https://danbooru.donmai.us/posts/"<>Integer.to_string(List.first(data)["id"])
+            @danbooru_colour_embed
+            |> title("<"<>"https://danbooru.donmai.us/posts/"<>Integer.to_string(List.first(data)["id"])<>">")
+            |> image("https://danbooru.donmai.us"<>List.first(data)["file_url"])
         end
       {:error, e} ->
         IO.inspect(e);
