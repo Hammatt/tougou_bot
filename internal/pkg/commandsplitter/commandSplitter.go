6e53bdd4af96bdb0f2ed94d0ff316ab1597104e5
Alexander Hammatt <alexander.hammatt@protonmail.com>
2018-06-17 18:28:33 +1200

    wrote command splitter functionality

--- internal/pkg/commandsplitter/commandSplitter.go
+++ internal/pkg/commandsplitter/commandSplitter.go
@@ -1,10 +1,23 @@
 package commandsplitter
 
+import (
+	"strings"
+)
+
 /*SplitCommand : takes a "command" string and splits it into an array of size 2 where the
  * 0th index is "just the command" and the 1st index is "the query"
  */
 func SplitCommand(stringToSplit string) [2]string {
-	//TODO: take the first "word" (everything until the first space)
 	var splitCommand [2]string
+
+	i := strings.Index(stringToSplit, " ")
+
+	if i > 0 {
+		splitCommand[0] = stringToSplit[:i]
+		splitCommand[1] = stringToSplit[i+1:]
+	} else { //case where we recieve a command without a query.
+		splitCommand[0] = stringToSplit
+	}
+
 	return splitCommand
 }
