c237c5ceaf3f6797bd6778f01b594267026c78ae
Alexander Hammatt <alexander.hammatt@protonmail.com>
2018-06-17 19:53:12 +1200

    changed jisho command to send an embed.
    Added a util go file from github for easier embed stuff!
    Still need to create loops etc. to properly parse the jisho api results
    for when we have words with multiple definitions and/or readings

--- internal/pkg/jisho/jisho.go
+++ internal/pkg/jisho/jisho.go
@@ -1,13 +1,45 @@
 package jisho
 
 import (
+	"encoding/json"
 	"net/url"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/hammatt/tougou_bot/internal/pkg/apicaller"
 	"github.com/hammatt/tougou_bot/internal/pkg/commandsplitter"
+	"github.com/hammatt/tougou_bot/internal/pkg/util"
 )
+
+type JishoApiResult struct {
+	Meta struct {
+		Status int `json:"status"`
+	} `json:"meta"`
+	Data []struct {
+		IsCommon bool          `json:"is_common"`
+		Tags     []interface{} `json:"tags"`
+		Japanese []struct {
+			Word    string `json:"word"`
+			Reading string `json:"reading"`
+		} `json:"japanese"`
+		Senses []struct {
+			EnglishDefinitions []string      `json:"english_definitions"`
+			PartsOfSpeech      []string      `json:"parts_of_speech"`
+			Links              []interface{} `json:"links"`
+			Tags               []interface{} `json:"tags"`
+			Restrictions       []interface{} `json:"restrictions"`
+			SeeAlso            []interface{} `json:"see_also"`
+			Antonyms           []interface{} `json:"antonyms"`
+			Source             []interface{} `json:"source"`
+			Info               []interface{} `json:"info"`
+		} `json:"senses"`
+		Attribution struct {
+			Jmdict   bool `json:"jmdict"`
+			Jmnedict bool `json:"jmnedict"`
+			Dbpedia  bool `json:"dbpedia"`
+		} `json:"attribution"`
+	} `json:"data"`
+}
 
 func jishoAPISearch(s string) (string, error) {
 	searchURL := "https://jisho.org/api/v1/search/words?keyword=\"" + url.QueryEscape(s) + "\""
@@ -18,6 +50,12 @@ func jishoAPISearch(s string) (string, error) {
 	}
 
 	return result, nil
+}
+
+func parseJishoAPIJSONResultToStruct(apiResult string) JishoApiResult {
+	var result JishoApiResult
+	json.Unmarshal([]byte(apiResult), &result)
+	return result
 }
 
 //CommandHandler : to be added as a handler by the discordgo library to handle the !jisho command
@@ -36,6 +74,13 @@ func CommandHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 			s.ChannelMessageSend(m.ChannelID, "jisho search error: "+err.Error())
 			return
 		}
-		s.ChannelMessageSend(m.ChannelID, jishoAPIResult)
+		//take the first result from the api.
+		jishoResult := parseJishoAPIJSONResultToStruct(jishoAPIResult).Data[0]
+
+		embed := util.NewEmbed().
+			AddField("Reading(s)", jishoResult.Japanese[0].Reading).
+			AddField("Definition(s):", jishoResult.Senses[0].EnglishDefinitions[0]).
+			SetColor(0xff0000).MessageEmbed
+		s.ChannelMessageSendEmbed(m.ChannelID, embed)
 	}
 }
